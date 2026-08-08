b3fb7f0f028275aff588b3f6c31b480f8ae8c789
MMahmud7673 <mahmudmirsaidov@933gmail.com>
2026-08-08 15:30:46 +0500

    added layout.ejs file in view folder

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const expressLayouts = require('express-ejs-layouts');
 const indexRouter = require('./routes/index');
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-app.set('layout', 'layouts/layout');
+app.set('layout', 'layouts/layouts');
 app.use(expressLayouts);
 app.use(express.static('public'));
 app.use('/', indexRouter);
