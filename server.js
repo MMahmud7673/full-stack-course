558120b7ade77f0e99fb807a909120e12d10c4fe
MMahmud7673 <mahmudmirsaidov@933gmail.com>
2026-08-08 18:42:51 +0500

    add mongoDB

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
@@ -8,4 +12,11 @@ app.set('layout', 'layouts/layouts');
 app.use(expressLayouts);
 app.use(express.static('public'));
 app.use('/', indexRouter);
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log("Connected to Mongoose!"));
+
 app.listen(process.env.PORT || 3001);
